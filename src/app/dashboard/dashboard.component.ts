4168d4084c273972de1760caf371a60f2eb4f94d
Olga Batsalai <olga.balatcay@cloudistics.com>
2019-03-22 19:19:57 +0200

    third commit

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,21 @@
-import { Component } from "@angular/core";
-
+import {Component, OnInit} from "@angular/core";
+import  { Storage} from "../storage";
+import {StorageService} from "../storage.service";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: [ './dashboard.component.css' ]
 })
- export class DashboardComponent {}
+ export class DashboardComponent implements OnInit{
+  storage: Storage[] = [];
+
+  constructor(private storageService: storageService){}
+  ngOnInit(){
+    this.getStorages();
+  }
+  getStorages(): void{
+    this.storageService.getStorages()
+      .subscribe(storages => this.storages = storages.slice(1,))
+  }
+}
